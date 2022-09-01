0db01540b4a615f12cf9ad3726ba2902558e7c44
Sara Schumer <sac.s@sbcglobal.net>
2022-09-01 15:46:51 -0500

    Hoisting
    Learned what hosting is and hosting functions

---
+++ Hoisting/hoisting-functions.js
@@ -0,0 +1,12 @@
+let paycheck = 300;
+
+function logNumberToConsole(num){
+    console.log(num);
+}
+
+function addRandomNumber(num){
+    let randomNumber - Math.random() * 100;
+    logNumberToConsole(randomNumber + num);
+}
+
+addRandomNumber(paycheck)
---
+++ Hoisting/hoisting.js
@@ -0,0 +1,10 @@
+//hoisting
+
+// x = 1;  reference error
+let x = 0;
+const y = 1;
+var z = 2;
+
+console.log(z);
+
+
